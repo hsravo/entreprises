dc0d20533551d276edd303c840b394f7eb983cc0
hsravo <hsravo1@gmail.com>
2021-01-27 20:11:56 +0100

    script fields fix

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -1,9 +1,8 @@
 class Charity < ApplicationRecord
 
-  validates :id_association, presence: { message: 'Le RNA doit être valide' }, uniqueness: { message: 'Le RNA est déjà utilisé' }
-  
-  validates :name, presence: { message: 'Le nom doit être renseigné ou est déjà utilisé' }, uniqueness: true
-  validates :acronym, presence: { message: 'Le nom court doit être renseigné ou est déjà utilisé' }, uniqueness: true
+  validates :id_association, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :acronym, presence: true, uniqueness: true
   
 
   # Include default devise modules. Others available are:
